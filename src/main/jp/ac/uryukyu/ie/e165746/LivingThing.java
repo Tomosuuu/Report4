f5aee41655ae0c3a398413ab3ae827b1a3b147c5
Tomotaka Yonaha <e165746@ie.u-ryukyu.ac.jp>
2016-11-24 17:23:10 +0900

    third commit  create LivingThing and updated Enemy,Hero

---
+++ src/main/jp/ac/uryukyu/ie/e165746/LivingThing.java
@@ -0,0 +1,44 @@
+package jp.ac.uryukyu.ie.e165746;
+
+/**
+ * Created by e165746 on 2016/11/24.
+ */
+public class LivingThing {
+    String name;
+    int hitPoint;
+    int attack;
+    boolean dead;
+
+    public LivingThing(String name, int maximumHP, int attack) {
+        this.name = name;
+        hitPoint = maximumHP;
+        this.attack = attack;
+        dead = false;
+        System.out.printf("%sのHPは%d。攻撃力は%dです。\n", name, maximumHP, attack);
+    }
+
+    public boolean isDead() {
+        return dead;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void attack(LivingThing opponent) {
+        if (dead == false) {
+            int damage = (int) (Math.random() * attack);
+            System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, opponent.getName(), damage);
+            opponent.wounded(damage);
+        }
+    }
+
+    public void wounded(int damage) {
+        hitPoint -= damage;
+        if (hitPoint < 0) {
+            dead = true;
+            information();
+        }
+    }
+    public void information(){}
+}
